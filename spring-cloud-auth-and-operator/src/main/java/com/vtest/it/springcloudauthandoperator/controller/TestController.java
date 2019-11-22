dd9c12b7dced4700bfe09b34db1b01502661f6b7
RoshanHuang <gitadmin@gmail.com>
2019-11-22 10:11:20 +0800

    xufeng.huang update Probercardsystem controller

--- spring-cloud-auth-and-operator/src/main/java/com/vtest/it/springcloudauthandoperator/controller/TestController.java
+++ spring-cloud-auth-and-operator/src/main/java/com/vtest/it/springcloudauthandoperator/controller/TestController.java
@@ -41,23 +41,29 @@ public class TestController {
         return "ROLE_NORMAL /normal/test接口调用成功！";
     }
 
+    @PreAuthorize("hasAuthority('ROLE_PTE-TOOLING') or hasAuthority('ROLE_PCR-TOOLING') or hasAuthority('ROLE_PCRLeader-TOOLING') or hasAuthority('ROLE_NORMAL-TOOLING') ")
     @GetMapping(value = "/AllList")
     public String getAllList(){
         return probercardInfoService.getAllList();
     }
 
+    @PreAuthorize("hasAuthority('ROLE_PTE-TOOLING') or hasAuthority('ROLE_PCR-TOOLING') or hasAuthority('ROLE_PCRLeader-TOOLING') or hasAuthority('ROLE_NORMAL-TOOLING') ")
     @GetMapping(value = "/SingleCard")
     public String getSingleCard(String cardId){
         return probercardInfoService.getSingleCard(cardId);
     }
+
+    @PreAuthorize("hasAuthority('ROLE_PTE-TOOLING') or hasAuthority('ROLE_PCR-TOOLING') or hasAuthority('ROLE_PCRLeader-TOOLING') or hasAuthority('ROLE_NORMAL-TOOLING') ")
     @GetMapping(value = "/ProberCardStatus")
     public String getProberCardStatus(String proberCardId){
         return probercardInfoService.getProberCardStatus(proberCardId);
     }
+    @PreAuthorize("hasAuthority('ROLE_PTE-TOOLING') or hasAuthority('ROLE_PCR-TOOLING') or hasAuthority('ROLE_PCRLeader-TOOLING')")
     @GetMapping(value = "/ReleaseCardInfo")
     public String getReleaseCardInfo(String proberCardId){
         return probercardInfoService.getReleaseCardInfo(proberCardId);
     }
+    @PreAuthorize("hasAuthority('ROLE_PTE-TOOLING') or hasAuthority('ROLE_PCR-TOOLING') or hasAuthority('ROLE_PCRLeader-TOOLING') or hasAuthority('ROLE_NORMAL-TOOLING') ")
     @GetMapping(value = "/AllProberCardStatus")
     public String getAllProberCardStatus(){
         return probercardInfoService.getAllProberCardStatus();
@@ -66,82 +72,104 @@ public class TestController {
     public boolean getProberCardReleaseFlag(String proberCardId){
         return probercardInfoService.getProberCardReleaseFlag(proberCardId);
     }
+    @PreAuthorize("hasAuthority('ROLE_PTE-TOOLING') or hasAuthority('ROLE_PCR-TOOLING') or hasAuthority('ROLE_PCRLeader-TOOLING') or hasAuthority('ROLE_NORMAL-TOOLING') ")
     @GetMapping(value = "/AllIQCRecord")
     public String getAllIQCRecord(){
         return probercardInfoService.getAllIQCRecord();
     }
 
+    @PreAuthorize("hasAuthority('ROLE_PTE-TOOLING') or hasAuthority('ROLE_PCR-TOOLING') or hasAuthority('ROLE_PCRLeader-TOOLING') or hasAuthority('ROLE_NORMAL-TOOLING') ")
     @GetMapping(value = "/AllMaintainRecord")
     public String getAllMaintainRecord(){
         return probercardInfoService.getAllMaintainRecord();
     }
+
+    @PreAuthorize("hasAuthority('ROLE_PCR-TOOLING') or hasAuthority('ROLE_PCRLeader-TOOLING')")
     @GetMapping(value = "/InfoRebuildCount")
     public Integer getInfoRebuildCount(String proberCardId){
         return probercardInfoService.getInfoRebuildCount(proberCardId);
     }
+    @PreAuthorize("hasAuthority('ROLE_PTE-TOOLING') or hasAuthority('ROLE_PCR-TOOLING') or hasAuthority('ROLE_PCRLeader-TOOLING') or hasAuthority('ROLE_NORMAL-TOOLING') ")
     @GetMapping(value = "/Td")
     public String getTd(){
         return probercardInfoService.getTd();
     }
+
+    @PreAuthorize("hasAuthority('ROLE_PCR-TOOLING') or hasAuthority('ROLE_PCRLeader-TOOLING')")
     @GetMapping(value = "/OutProberCard")
     public String getOutProberCard(String[] proberCardIdArrays){
         return probercardInfoService.getOutProberCard(proberCardIdArrays);
     }
+    @PreAuthorize("hasAuthority('ROLE_PCR-TOOLING') or hasAuthority('ROLE_PCRLeader-TOOLING')")
     @GetMapping(value = "/BackProberCard")
     public String getBackProberCard(String[] proberCardIdArrays){
         return probercardInfoService.getBackProberCard(proberCardIdArrays);
     }
+    @PreAuthorize("hasAuthority('ROLE_PCR-TOOLING') or hasAuthority('ROLE_PCRLeader-TOOLING')")
     @GetMapping(value = "/InfoProberCard")
     public String getInfoProberCard(String[] proberCardIdArrays){
         return probercardInfoService.getInfoProberCard(proberCardIdArrays);
     }
+    @PreAuthorize("hasAuthority('ROLE_PCR-TOOLING') or hasAuthority('ROLE_PCRLeader-TOOLING')")
     @GetMapping(value = "/IQCProberCard")
     public String getIQCProberCard(String[] proberCardIdArrays){
         return probercardInfoService.getIQCProberCard(proberCardIdArrays);
     }
+    @PreAuthorize("hasAuthority('ROLE_PCR-TOOLING') or hasAuthority('ROLE_PCRLeader-TOOLING')")
     @GetMapping(value = "/MaintainProberCard")
     public String getMaintainProberCard(String[] proberCardIdArrays){
         return probercardInfoService.getMaintainProberCard(proberCardIdArrays);
     }
+    @PreAuthorize("hasAuthority('ROLE_PCR-TOOLING') or hasAuthority('ROLE_PCRLeader-TOOLING')")
     @GetMapping(value = "/ReleaseProberCard")
     public String getReleaseProberCard(String[] proberCardIdArrays){
         return probercardInfoService.getReleaseProberCard(proberCardIdArrays);
     }
+    @PreAuthorize("hasAuthority('ROLE_PCR-TOOLING') or hasAuthority('ROLE_PCRLeader-TOOLING')")
     @GetMapping(value = "/ProberCardId")
     public String getProberCardId(String[] custNameArrays){
         return probercardInfoService.getProberCardId(custNameArrays);
     }
+    @PreAuthorize("hasAuthority('ROLE_PCR-TOOLING') or hasAuthority('ROLE_PCRLeader-TOOLING')")
     @GetMapping(value = "/EXRecord")
     public String getEXRecord(String[] proberCardIdArrays){
         return probercardInfoService.getEXRecord(proberCardIdArrays);
     }
-    @GetMapping(value = "/ProberCardEX")
+    @PreAuthorize("hasAuthority('ROLE_PCRLeader-TOOLING')")
+    @GetMapping(value = "/EXProberCard")
     public String getProberCardEX(){
         return probercardInfoService.getProberCardEX();
     }
+
+    @PreAuthorize("hasAuthority('ROLE_PCRLeader-TOOLING')")
     @GetMapping(value = "/EXInfoSingle")
     public String getEXInfoSingle(String proberCardId){
         return probercardInfoService.getEXInfoSingle(proberCardId);
     }
+    @PreAuthorize("hasAuthority('ROLE_PTE-TOOLING') or hasAuthority('ROLE_PCR-TOOLING') or hasAuthority('ROLE_PCRLeader-TOOLING') or hasAuthority('ROLE_NORMAL-TOOLING') ")
     @GetMapping(value = "/AllIQCRecordByMinTime")
     public String getAllIQCRecordByMinTime(){
         return probercardInfoService.getAllIQCRecordByMinTime();
     }
+    @PreAuthorize("hasAuthority('ROLE_PTE-TOOLING') or hasAuthority('ROLE_PCR-TOOLING') or hasAuthority('ROLE_PCRLeader-TOOLING') or hasAuthority('ROLE_NORMAL-TOOLING') ")
     @GetMapping(value = "/AllIQCRecordByMaxTime")
     public String getAllIQCRecordByMaxTime(){
         return probercardInfoService.getAllIQCRecordByMaxTime();
     }
 
+    @PreAuthorize("hasAuthority('ROLE_PCR-TOOLING') or hasAuthority('ROLE_PCRLeader-TOOLING')")
     @PostMapping(value = "/ProberCardInfo")
     public void addProberCardInfo(@RequestBody ProberCardEntityBean bean){
           operatorService.addProberCardInfo(bean);
     }
+
+    @PreAuthorize("hasAuthority('ROLE_PCR-TOOLING') or hasAuthority('ROLE_PCRLeader-TOOLING')")
     @PostMapping(value = "/NewIqcRecord")
     public void addNewIqcRecord(@RequestBody IqcRecordBean bean){
         operatorService.addNewIqcRecord(bean);
     }
 
-    @PreAuthorize("!hasAuthority('ROLE_NORMAL-TOOLING')")
+    @PreAuthorize("hasAuthority('ROLE_PCR-TOOLING') or hasAuthority('ROLE_PCRLeader-TOOLING')")
     @PostMapping(value = "/File")
     public void putFile(@RequestParam(value = "excelFile", required = false) MultipartFile file, String proberCardId) {
         String descPath = "D:/upload/" +proberCardId;
@@ -158,68 +186,85 @@ public class TestController {
             }
         }
     }
+    @PreAuthorize("hasAuthority('ROLE_PCR-TOOLING') or hasAuthority('ROLE_PCRLeader-TOOLING')")
     @PostMapping(value = "/outProberCard")
     public void outProberCard(@RequestBody OutProberCardBean bean){
         operatorService.outProberCard(bean);
     }
+    @PreAuthorize("hasAuthority('ROLE_PCR-TOOLING') or hasAuthority('ROLE_PCRLeader-TOOLING')")
     @PostMapping(value = "/backProberCard")
     public void backProberCard(@RequestBody BackProberCardBean bean){
         operatorService.backProberCard(bean);
     }
+    @PreAuthorize("hasAuthority('ROLE_PTE-TOOLING') or hasAuthority('ROLE_PCR-TOOLING') or hasAuthority('ROLE_PCRLeader-TOOLING')")
     @PostMapping(value = "/ProberCardState")
     public void updateProberCardState(String proberCardId, String newStatus, String oldStatus, String operator){
            operatorService.updateProberCardState(proberCardId,newStatus,oldStatus,operator);
     }
+    @PreAuthorize("hasAuthority('ROLE_PCR-TOOLING') or hasAuthority('ROLE_PCRLeader-TOOLING')")
     @PostMapping(value = "/ProbercardCheck")
     public void checkProberCard(@RequestBody ReleaseProberCardBean bean){
         operatorService.checkProberCard(bean);
     }
+    @PreAuthorize("hasAuthority('ROLE_PTE-TOOLING')")
+    @PostMapping(value = "/PTECheck")
+    public void PTECheck(@RequestBody ReleaseProberCardBean bean){
+        operatorService.checkProberCard(bean);
+    }
+    @PreAuthorize("hasAuthority('ROLE_PCRLeader-TOOLING')")
     @PostMapping(value = "/ReleaseProberCard")
     public void ReleaseProberCard(@RequestBody ReleaseProberCardBean bean){
         operatorService.ReleaseProberCard(bean);
     }
+    @PreAuthorize("hasAuthority('ROLE_PCR-TOOLING') or hasAuthority('ROLE_PCRLeader-TOOLING')")
     @PostMapping(value = "/ProberCardMaintain")
     public void ProberCardMaintain(@RequestBody ProberCardMaintainBean bean){
         operatorService.ProberCardMaintain(bean);
     }
+    @PreAuthorize("hasAuthority('ROLE_PCRLeader-TOOLING')")
     @PostMapping(value = "/ProberCardEX")
     public void addProberCardEX(@RequestBody ProberCardExtensionBean bean){
         operatorService.addProberCardEX(bean);
     }
 
-    @PreAuthorize("!hasAuthority('ROLE_NORMAL-TOOLING') or !hasAuthority('ROLE_PTE-TOOLING')")
+    @PreAuthorize("hasAuthority('ROLE_PCR-TOOLING') or hasAuthority('ROLE_PCRLeader-TOOLING')")
     @PostMapping("/ProberCard")
     public void updateProberCard(@RequestBody ProberCardEntityBean bean) {
         operatorService.updateProberCard(bean);
     }
+    @PreAuthorize("hasAuthority('ROLE_PCR-TOOLING') or hasAuthority('ROLE_PCRLeader-TOOLING')")
     @PostMapping("/ProberCardReleaseFlag")
     public void updateProberCardReleaseFlag(String proberCardId, boolean releaseFlag)  {
         operatorService.updateProberCardReleaseFlag(proberCardId,releaseFlag);
     }
 
-    @PreAuthorize("!hasAuthority('ROLE_NORMAL-TOOLING') or !hasAuthority('ROLE_PTE-TOOLING')")
+    @PreAuthorize("hasAuthority('ROLE_PCR-TOOLING') or hasAuthority('ROLE_PCRLeader-TOOLING')")
     @PostMapping("/ProberCardInfoReleaseFlag")
     public void updateProberCardInfoReleaseFlag(String proberCardId, boolean releaseFlag) {
         operatorService.updateProberCardInfoReleaseFlag(proberCardId,releaseFlag);
     }
 
-    @PreAuthorize("!hasAuthority('ROLE_NORMAL-TOOLING') or !hasAuthority('ROLE_PTE-TOOLING')")
+    @PreAuthorize("hasAuthority('ROLE_PCR-TOOLING') or hasAuthority('ROLE_PCRLeader-TOOLING')")
     @PostMapping("/SingleState")
     public void updateSingleState(String proberCardId, String currentProcess){
         operatorService.updateSingleState(proberCardId,currentProcess);
     }
+    @PreAuthorize("hasAuthority('ROLE_PCR-TOOLING') or hasAuthority('ROLE_PCRLeader-TOOLING')")
     @PostMapping("/ProberCardItem")
     public void clearProberCardItem(String proberCardId, String pinlenSpec, String pindiamSpec, String pinlevelSpec, Integer rebuildCount) {
         operatorService.clearProberCardItem(proberCardId,pinlenSpec,pindiamSpec,pinlevelSpec,rebuildCount);
     }
+    @PreAuthorize("hasAuthority('ROLE_PCR-TOOLING') or hasAuthority('ROLE_PCRLeader-TOOLING')")
     @PostMapping("/MaintainItem")
     public void clearMaintainItem(String proberCardId, double afterPinlen, double afterPindiam, double afterPinlevel) {
         operatorService.clearMaintainItem(proberCardId, afterPinlen, afterPindiam, afterPinlevel);
     }
+    @PreAuthorize("hasAuthority('ROLE_PCR-TOOLING') or hasAuthority('ROLE_PCRLeader-TOOLING')")
     @PostMapping("/IQCItem")
     public void clearIQCItem(String proberCardId, double pinMinlen, double pinMaxdiam, double pinLevel) {
         operatorService.clearIQCItem(proberCardId, pinMinlen, pinMaxdiam, pinLevel);
     }
+    @PreAuthorize("hasAuthority('ROLE_PCR-TOOLING') or hasAuthority('ROLE_PCRLeader-TOOLING')")
     @PostMapping("/PM")
     public void  cleanPM(String cardid,String ownerid) {
         operatorService.cleanPM(cardid, ownerid);
